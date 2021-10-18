4f6fee379d1e5ac5c9a9730e1fd73f9009b638a6
Romain <romain.desson.02320@gmail.com>
2021-10-18 12:08:08 +0200

    init project

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f4109c531b8fc907cd9d6608084a9b69'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '997c2207-a4ce-4432-99f0-a2380cd9306a'
+};
